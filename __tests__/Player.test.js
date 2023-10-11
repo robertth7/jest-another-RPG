8ed9de536ef2a1141eb50aa396a6c9f7acca2c7c
Roberto Hurtado Jr <rhurtado.267@gmail.com>
2023-10-11 14:00:49 -0500

    adding prototype to Player

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -17,3 +17,26 @@ test('creates a player object', () => {
         expect.arrayContaining([expect.any(Object)])
     );
 });
+
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+// here, we're checking that player.getStats() returns an object with four specific properties. 
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+});
+
+// on player creation, the inventory should already have something in it, so a call to player.getInventory() should return an array
+// there's also the case of an empty inventory needing to return false. you can simulate an empty array yourself by setting player.inventory = [] before the next expect() runs. 
